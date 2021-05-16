b3a158fa75531f66a6b68a423ab287b6ced9e3c9
Juliana <juliana.salafia@gmail.com>
2021-05-16 18:50:27 -0300

    dto, request e response [incompleto]

---
+++ src/main/java/one/digitalinnovation/personAPI/dto/request/PersonDTO.java
@@ -0,0 +1,22 @@
+package one.digitalinnovation.personAPI.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class PersonDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String cpf;
+    private String birthDate;
+    private List<PhoneDTO> phones;
+}
