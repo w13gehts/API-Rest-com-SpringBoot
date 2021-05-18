32a1dff7c2a976bb53d77433673eaeacf31f254c
Juliana <juliana.salafia@gmail.com>
2021-05-18 20:22:42 -0300

    developed find by id and  delete by id operation

--- src/main/java/one/digitalinnovation/personAPI/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personAPI/controller/PersonController.java
@@ -2,11 +2,13 @@ package one.digitalinnovation.personAPI.controller;
 
 import one.digitalinnovation.personAPI.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personAPI.dto.request.PersonDTO;
+import one.digitalinnovation.personAPI.exception.PersonNotFoundException;
 import one.digitalinnovation.personAPI.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping ("/api/v1/people")
@@ -23,5 +25,21 @@ public class PersonController {
     @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
         return personService.createPerson(personDTO);
+    }
+
+    @GetMapping
+    public List<PersonDTO> listAll() {
+        return personService.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
+        return personService.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) throws PersonNotFoundException {
+        personService.delete(id);
     }
 }
--- src/main/java/one/digitalinnovation/personAPI/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personAPI/service/PersonService.java
@@ -1,11 +1,15 @@
 package one.digitalinnovation.personAPI.service;
-import one.digitalinnovation.personAPI.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personAPI.dto.request.PersonDTO;
+import one.digitalinnovation.personAPI.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personAPI.entity.Person;
+import one.digitalinnovation.personAPI.exception.PersonNotFoundException;
 import one.digitalinnovation.personAPI.mapper.PersonMapper;
 import one.digitalinnovation.personAPI.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
@@ -27,5 +31,27 @@ public class PersonService {
                     .builder()
                     .message("Created person with ID " + savedPerson.getId())
                     .build();
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    public PersonDTO findById(Long id) throws PersonNotFoundException {
+       Person person = VerifyIfExists(id);
+        return personMapper.toDTO(person);
+    }
+
+    private Person VerifyIfExists(Long id) throws PersonNotFoundException {
+        return personRepository.findById(id)
+                .orElseThrow(() -> new PersonNotFoundException(id));
+    }
+
+    public void delete(Long id) throws PersonNotFoundException {
+        VerifyIfExists(id);
+        personRepository.deleteById(id);
     }
 }
